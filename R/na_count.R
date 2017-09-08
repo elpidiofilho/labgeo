c3b936b06da3ec203826f0d52d3f710bc20a5015
Elpidio Filho <elpidiofilho@gmail.com>
2017-09-07 21:42:11 -0300

    ajustes nos gráficos

--- R/na_count.R
+++ R/na_count.R
@@ -16,33 +16,34 @@
 
 
 na_count <- function(df, verbose = TRUE){
- classe <- na_absolute <- na_relative <- variav <- NULL
+  classe <- na_absolute <- na_relative <- variav <- classe_NA  <- NULL
   nc = ncol(df)
   na_count <- data.frame(variav = character(nc),
-                        class_var = character(nc),
-                        na_absolute = integer(nc),
-                        na_relative = numeric(nc),
-                        uniques_values = integer(nc),
-                        stringsAsFactors = FALSE)
+                         class_var = character(nc),
+                         na_absolute = integer(nc),
+                         na_relative = numeric(nc),
+                         uniques_values = integer(nc),
+                         classe_NA = factor(nc),
+                         stringsAsFactors = FALSE)
   na_count$variav <- names(df)
   na_count$class_var<- sapply(df, function(y) class(y))
   na_count$na_absolute <- sapply(df, function(y) sum(length(which(is.na(y)))))
-  na_count$na_relative <- na_count$na_absolute / nrow(df)
+  na_count$na_relative <- na_count$na_absolute / nrow(df) * 100
   na_count$uniques_values <- sapply(df, function(y) length(unique(y)))
-  na_count$classe <- cut((na_count$relative+0.001) * 100, c(0,25,50,75,100), dig.lab = 4)
+  na_count$classe_NA <- cut((na_count$na_relative + 0.001), breaks = c(0,25,50,75,100))
 
   if (verbose == TRUE) {
     nr = nrow(df)
     g1 = ggplot2::ggplot(data = na_count, ggplot2::aes(x = stats::reorder(variav, na_relative),
-                                                  y = na_relative * 100,
-                                                  fill = as.factor(classe))) +
+                                                       y = na_relative,
+                                                       fill = as.factor(classe_NA))) +
       ggplot2::geom_col() + ggplot2::labs(fill='% NA') +
-      ggplot2::xlab("variaveis") + ggplot2::ylab('% NA') +
+      ggplot2::xlab("Variables") + ggplot2::ylab('% NA') +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
 
     g2 = ggplot2::ggplot(data = na_count, ggplot2::aes(x = stats::reorder(variav, nr - na_absolute),
-                                                  y = nr - na_absolute, fill= as.factor(classe))) +
-      ggplot2::geom_col() + ggplot2::ylab('Amostras validas') + ggplot2::xlab("variaveis") +
+                                                       y = nr - na_absolute, fill= as.factor(classe_NA))) +
+      ggplot2::geom_col() + ggplot2::ylab('Valid Samples') + ggplot2::xlab("Variables") +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)) +
       ggplot2::labs(fill='% NA')
     print(g1)
@@ -51,3 +52,4 @@ na_count <- function(df, verbose = TRUE){
 
   return(na_count)
 }
+
