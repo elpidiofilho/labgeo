c2d9b97194d3df514fc7b88c26680790f4c9f06a
Elpidio Filho <elpidiofilho@gmail.com>
2017-10-22 00:46:30 -0200

    add stacktopredict

--- R/predict_model.R
+++ R/predict_model.R
@@ -47,3 +47,50 @@ predict_to_map <- function(model_list, path_raster, raster_type = ".asc",
     print(paste( "time prediction", hms_span(inicio, Sys.time())))
   }
 }
+
+
+
+#' predict stack to map
+#'
+#' This function predict a model fit and save result as a map raster file
+#'
+#'
+#' @param model_list list of fit models by function run_models
+#' @param stack_var Stack with all vars used to fit them model
+#' @param path_result path to store maps with results of prediction
+#' @param namefile sufix of name of file with results of prediction
+#' @param result_type type of file (extension) to be saved after prediction
+#' @keywords predict models maps raster
+#' @details  details
+#' @author Elpidio Filho, \email{elpidio@ufv.br}
+#' @examples
+#' \dontrun{
+#' predict_stack_to_map(fit.models,
+#'               stack_var = predicao,
+#'               path_result = './result/',
+#'               namefile = 'uso_',
+#'               result_type = ".tif")
+#' }
+#' @export
+
+predict_stack_to_map <- function(model_list, stack_var,
+                                 path_result, namefile, result_type= ".tif") {
+  nm <- length(model_list)
+  for (i in 1:nm) {
+    inicio <- Sys.time()
+    vsel <- model_list[[i]]$coefnames
+    vachei <- !((names(stack_var)) %in% vsel)
+    if (sum(vachei) > 0) {
+      print(paste(names(stack_var)[vachei]))
+      stop("covariate no found")
+    }
+    name_model <- model_list[[i]]$modelInfo$label
+    filename <- gsub(" ", "_", paste0(path_result, namefile,
+                                      name_model, result_type))
+    print(paste("model : ", name_model, "file name", filename))
+    raster::predict(object = stack_var, model = model_list[[i]], na.rm = T,progress="text",
+                    filename = filename, overwrite = TRUE)
+    print(paste( "time prediction", hms_span(inicio, Sys.time())))
+    endCluster()
+  }
+}
