097c803d951b697f9aa26867f328adf223bb208e
elpidiofilho <elpidiofilho@gmail.com>
2017-10-27 13:58:32 -0200

    bugs

--- R/predict_model.R
+++ R/predict_model.R
@@ -30,7 +30,7 @@ predict_to_map <- function(model_list, path_raster, raster_type = ".asc",
   nm <- length(model_list)
   for (i in 1:nm) {
     inicio <- Sys.time()
-    vsel <- model_list[[i]]$coefnames
+    vsel <- model_list$fit[[i]]$coefnames
     var_file_raster <- paste0(path_raster, vsel, raster_type)
     st <- raster::stack(var_file_raster)
     vachei <- !(names(st) %in% vsel)
