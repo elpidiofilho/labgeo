8a0ed6a9d7d6ebd73ab8d25c707ff6925fa381e1
Elpidio Filho <elpidiofilho@gmail.com>
2017-11-01 00:39:30 -0200

    bugs correction

--- R/predict_big_raster.R
+++ R/predict_big_raster.R
@@ -13,6 +13,7 @@
 #' @param cpu_cores numeber of cpu cores
 #' @param verbose verbose
 #' @importFrom progress progress_bar
+#' @importFrom raster stack beginCluster endCluster clusterR writeRaster crop res mosaic raster
 #' @return
 #' @export
 #'
@@ -23,14 +24,14 @@
 
 predict_big_raster <- function(model, st, path_file, suffix = 'pred_',
                                format = "GTiff", tiles = 5, cpu_cores = 7, verbose = TRUE) {
-  td = NULL
-  td = create_temp_dir()
+  td <- NULL
+  td <- create_temp_dir()
   tile_stack(st = st, dir = td, num_tiles = tiles)
-  i = 1
+  i <- 1
 
   for (i in 1:length(model)) {
-    nm = model[[i]]$modelInfo$label
-    tile_predict(model = model[[i]], dir = td, cpu_cores = cpu_cores, verbose = verbose)
+    nm <- model[[i]]$modelInfo$label
+    tile_predict(model <- model[[i]], dir = td, cpu_cores = cpu_cores, verbose = verbose)
     mosaic_tiles(dir = td,  name_mosaic = paste0(path_file, suffix, nm, '.tif'), format = "GTiff")
   }
   if (dir.exists(td)) {
@@ -50,7 +51,7 @@ create_temp_dir <- function() {
 }
 
 #st = predicao; dir = td; num_tiles = 5
-tile_stack <- function(st, dir, num_tiles) {
+tile_stack <- function(st, dir, num_tiles, verbose = TRUE) {
   n.side <-  num_tiles  # number of tiles per side
   dx     <- (extent(predicao)[2]- extent(predicao)[1])/ n.side  # extent of one tile in x direction
   dy     <- (extent(predicao)[4]- extent(predicao)[3])/ n.side  # extent of one tile in y direction
@@ -59,13 +60,24 @@ tile_stack <- function(st, dir, num_tiles) {
   cS     <- expand.grid(x= xs, y= ys)
   ## loop over extents and crop
   i = 1
+  nr = nrow(cS)
+  if (verbose == TRUE) {
+    pb <- progress_bar$new(total = nr,
+                           format("Running [:bar] :percent elapsed: :elapsed eta: :eta"),
+                           clear = FALSE)
+
+  }
   inicio = Sys.time()
   print("creating tiles")
   for(i in 1:nrow(cS)) {
     #  for(i in 1:4) {
-    ex1 <- c(cS[i,1], cS[i,1]+dx, cS[i,2], cS[i,2]+dy)
+    ex1 <- c(cS[i,1], cS[i,1] + dx, cS[i,2], cS[i,2] + dy)
     cl1 <- crop(predicao, ex1)
     writeRaster(x = cl1, filename = paste0(dir,'/tiles/tl_', i,".grd"), format="raster", overwrite=T)
+    if (verbose == TRUE) {
+      pb$tick()
+    }
+
   }
   print(paste("tile", hms_span(inicio,Sys.time())))
 
@@ -81,7 +93,7 @@ tile_predict <- function(model, dir, cpu_cores =  4, verbose = TRUE) {
   nr <- length(rasters0)
   inicio <- Sys.time()
   if (verbose == TRUE) {
-    pb <- progress_bar$new(total = length(model) * num_tiles,
+    pb <- progress_bar$new(total = nr,
                            format("Running [:bar] :percent elapsed: :elapsed eta: :eta"),
                            clear = FALSE)
 
@@ -98,7 +110,7 @@ tile_predict <- function(model, dir, cpu_cores =  4, verbose = TRUE) {
       writeRaster(rna, filename = paste0(dir_predict, s1, '.tif'), format = "GTiff", overwrite = T )
     } else {
       beginCluster(cpu_cores)
-      pred <- clusterR(rt, predict, args = list(modelo_calibra),
+      pred <- clusterR(rt,  raster::predict, args = list(modelo_calibra),
                        filename = paste0(dir_predict, s1, '.tif'), format = "GTiff",
                        progress = T, overwrite = T)
       endCluster()
@@ -123,7 +135,7 @@ mosaic_tiles <- function(dir, name_mosaic, format = "GTiff" ) {
     rast.list[i] <- list(raster(rasters1[i]))
   }
   rast.list$fun <- mean
-  rast.mosaic <- do.call(mosaic, rast.list)
+  rast.mosaic <- do.call(raster::mosaic, rast.list)
   plot(rast.mosaic)
   writeRaster(rast.mosaic, filename = name_mosaic, format = format, overwrite = TRUE   )
   print("end")
