ca0337d118d8dfd578d648b9e21405fdb3a19bc8
elpidiofilho <elpidiofilho@gmail.com>
2018-04-23 08:02:41 -0300

    data wrangler

--- R/data_wrangler.R
+++ R/data_wrangler.R
@@ -1,9 +1,38 @@
+## https://gist.github.com/romainfrancois
 back <- function(data, ...) {
-  dots <- quos(...)
-  ndots <- map(dots, function(q) expr(-!!q))
-  select(data, !!!ndtos, !!!dots)
+  dots <- dplyr::quos(...)
+  ndots <-  purrr::map(dots, function(q) expr(-!!q))
+  dplyr::select(data, !!!ndtos, !!!dots)
 }
 
 front <- function(data, ...) {
-  select(data, ..., everything())
+  dplyr::select(data, ..., everything())
+}
+
+lags <- function(var, n = 3){
+  var <- enquo(var)
+
+  indices <- seq_len(n)
+  map( indices, ~quo(lag(!!var, !!.x)) ) %>%
+    set_names(sprintf("lag_%s_%02d", quo_text(var), indices))
+
+}
+
+
+na_set <- function(x, p){
+  p <- as_mapper(p)
+  x[p(x)] <- NA
+  x
+}
+
+naset_all <- function(data, p){
+  mutate_all(data, funs(na_set(., p)) )
+}
+
+naset_at <- function(data, at, p){
+  mutate_at(data, at, funs(na_set(., p)) )
+}
+
+naset_if <- function(data, test, p){
+  mutate_if(data, test, funs(na_set(., p)) )
 }
